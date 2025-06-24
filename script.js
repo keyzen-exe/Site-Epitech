0fbd6c000eabcb63d58eba6795b80bc3cb63961d
keyzen-exe <gazi.sfc@gmail.com>
2025-06-24 10:46:29 +0200

    maj

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ AOS.init();
 function toggleCheck(el) {
     el.classList.toggle('selected');
   }
+
+
+  var map = L.map('map').setView([45.74188, 4.84179], 16);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([45.74188, 4.84179]).addTo(map)
+    .bindPopup('Eden Agence Lyon 7ème <br>2 Rue du Professeur Charles Appleton, 69007 Lyon')
+    .openPopup();
